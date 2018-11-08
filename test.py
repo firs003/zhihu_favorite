8d2f4bec6fd7f98714888233bb8dd3d6751c0c8e
sleng <firs003@qq.com>
2018-11-08 17:14:52 +0800

    Add simple demo of bs4 json function list and dict

--- test.py
+++ test.py
@@ -1,7 +1,42 @@
 #!/usr/bin/python
 #coding=utf-8
 
+import os
 import requests
+import json
+from bs4 import BeautifulSoup
+
+def get_json_keys(json):
+	keys = []
+	for key in json:
+		keys.append(key)
+	return keys
+
+def get_title_list(json, keys, title):
+	names = []
+	for key in keys:
+		names.append(json[key][title])
+	return names
+
+def mkdir_by_title(titles):
+	parent = 'zhihu/'
+	os.mkdir(parent)
+	for title in titles:
+		os.mkdir(parent + title)
+
+def get_url_list(json, keys):
+	urls = []
+	pre = 'https://www.zhihu.com/collection/'
+	for key in keys:
+		urls.append(pre + key)
+	return urls
+
+def get_title_url_list(json):
+	pre = 'https://www.zhihu.com/collection/'
+	tus = []
+	for key in json:
+		tus.append({'title':json[key]['title'], 'url':pre + key})
+	return tus
 
 print "你好，世界"
 
@@ -11,4 +46,27 @@ headers = {
 }
 
 firs003_favourite = requests.get("https://www.zhihu.com/people/firs003/collections/", headers=headers)
-print firs003_favourite.content
+# print firs003_favourite.content
+soup = BeautifulSoup(firs003_favourite.content, features="html.parser")
+# <script id="js-initialData" type="text/json">
+json_all_str = soup.find('script', attrs={'id':'js-initialData', 'type':'text/json'}).get_text()
+# json_all = soup_init.get_text()
+json_all = json.loads(json_all_str)
+json_favlists = json_all['initialState']['entities']['favlists']
+# print json.JSONEncoder(json_favlists)
+# print json_favlists
+
+keys = get_json_keys(json_favlists)
+titles = get_title_list(json_favlists, keys, 'title')
+urls = get_url_list(json_favlists, keys)
+# print urls
+# mkdir_by_title(titles)
+title_url_list = get_title_url_list(json_favlists)
+for tu in title_url_list:
+	print tu['title'] + ':' + tu['url']
+# for i in range(len(keys)):
+# 	# print json_favlists[key]['title']
+# 	print str(keys[i]) + json_favlists[keys[i]] + titles[i] + ':' + urls[i]
+ 	
+fav = requests.get("https://www.zhihu.com/collection/102142765", headers=headers)
+print fav.content
